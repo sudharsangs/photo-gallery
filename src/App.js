e116baf69ad755a59240eec10e53d58cc2b03392
Sudharsan G S <g.s.sudharsan99@gmail.com>
2020-04-11 19:29:05 +0530

    Added responsiveness

--- src/App.js
+++ src/App.js
@@ -20,11 +20,11 @@ function App() {
     fetchImage();
   },[term])
   return (
-      <div className="container mx-auto">
+      <div className="container mx-auto sm:w-full">
         <ImageSearch searchText={(text) => setTerm(text)} />
         {!isLoading && images.length === 0&& <h1 className="text-6xl text-center mx-auto mt-32">No Images Found</h1>}
         {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading!</h1> : 
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid md:grid-cols-3 sm:grid-cols-1 gap-4">
           {images.map(image => (
             <ImageCard key={image.id} image={image}/>
           ))}
