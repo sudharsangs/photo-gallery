5a55d09d44c612c5c8ee10c071bd3ca1d014d293
Sudharsan G S <g.s.sudharsan99@gmail.com>
2020-04-11 18:27:27 +0530

    Done with fetching

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+import ImageCard from './components/ImageCard';
 
 function App() {
+  const [images,setImages] = useState([]);
+  const [isLoading,setIsLoading] = useState(true);
+  const [term,setTerm] = useState('');
+
+  const url = `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+  const fetchImage = async () => {
+    const response = await axios.get(url);
+    setImages(response.data.hits);
+    setIsLoading(false);
+    
+  }
+
+  useEffect(() => {
+    fetchImage();
+  })
   return (
-    <div>
-      <h1 className="text-2xl">Hello!</h1>
-    </div>
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-4">
+          {images.map(image => (
+            <ImageCard key={image.id} image={image}/>
+          ))}
+        </div> 
+      </div>
   );
 }
 
