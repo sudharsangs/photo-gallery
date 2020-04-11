5e184c66e2de3ba6ea90f661057d0bb730a7fd4c
Sudharsan G S <g.s.sudharsan99@gmail.com>
2020-04-11 18:56:41 +0530

    Added no data found

--- src/App.js
+++ src/App.js
@@ -18,10 +18,11 @@ function App() {
 
   useEffect(() => {
     fetchImage();
-  })
+  },[term])
   return (
       <div className="container mx-auto">
         <ImageSearch searchText={(text) => setTerm(text)} />
+        {!isLoading && images.length && <h1 className="text-6xl text-center mx-auto mt-32">No Images Found</h1>}
         {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading!</h1> : 
         <div className="grid grid-cols-3 gap-4">
           {images.map(image => (
